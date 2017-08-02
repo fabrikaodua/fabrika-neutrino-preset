badd4893856462485f2940425f8e7a16ec82c86b
constgen <const.gen@gmail.com>
2017-08-03 02:45:08 +0300

    Add Vue ESlint linting

--- presets/eslint/eslint-preset.js
+++ presets/eslint/eslint-preset.js
@@ -1,40 +1,20 @@
 'use strict'
 
 let airbnbPreset = require('neutrino-preset-airbnb-base')
-let arrify = require('arrify')
-let merge = require('deepmerge')
 
 let eslintConfig = require('./eslint.config.js')
+let eslintVueMiddleware = require('../../middlewares/middleware-eslint-vue.js')
 
 module.exports = function (neutrino) {
-	let config = neutrino.config
-	let lintRule = config.module.rule('lint')
-	const EXTENSIONS = arrify(lintRule.get('test')).concat([/\.vue$/])
-
 	neutrino.use(airbnbPreset, {
 		eslint: eslintConfig
 	})
-
-	config.module
-		.rule('lint')
-		.test(EXTENSIONS)
-		.use('eslint')
-			.tap(options => merge(options, {
-				plugins: ['vue'],
-				envs: { node: true },
-				rules: {
-					'vue/jsx-uses-vars': 2
-				}
-			}))
-
-
-	// config.module
-	//    .rule('lint')
-	//    .use('eslint')
-	//    .tap(options => {
-	// 		options.parser = 'vue-eslint-parser'
-	// 		options.parserOptions.parser = 'babel-eslint'
-	// 		options.parserOptions.sourceType = 'module'
-	// 		return options
-	//    });
+	neutrino.use(eslintVueMiddleware, {
+		eslint: {
+			rules: {
+				'vue/html-no-self-closing': 'off',
+				'vue/no-invalid-template-root': 'off'
+			}
+		}
+	})
 }
--- presets/eslint/eslint.config.js
+++ presets/eslint/eslint.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-	"rules": {
-		"id-length": ["error", { "min": 2, "properties": 'never', "exceptions": ['i'] }],
-		"max-len": ["error", 120, {"ignoreComments": true}],
-		"quote-props": [1, "consistent-as-needed"],
-		"eol-last": 0,
-		"comma-dangle": 0,
-		"linebreak-style": 0,
-		"no-tabs": 0,
-		"indent": ["error", "tab"]
+	rules: {
+		'id-length': ['error', { min: 2, properties: 'never', exceptions: ['i'] }],
+		'max-len': ['error', 120, {ignoreComments: true}],
+		'quote-props': [1, 'consistent-as-needed'],
+		'eol-last': 0,
+		'comma-dangle': 0,
+		'linebreak-style': 0,
+		'no-tabs': 0,
+		'indent': ['error', 'tab']
 	}
 }
 
